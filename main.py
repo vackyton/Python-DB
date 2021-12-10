7f7bfb306b377ada1fa3a25c902d985e611d6c25
Huge Idiot (Vackyton) <vackytonplays@gmail.com>
2021-12-10 18:38:05 -0500

    Updated Code, added test commands and clear
    So it worked this time, @client.command didn't work and i found this solution on stackoverflow

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import discord
+from discord.ext import commands
 
 client = discord.Client(command_prefix="$")
 
-@client.event
-async def on_ready():
-  print("Bot is Working!")
 
+# when bot is online
+async def on_ready():
+    print('Logged in as ' + str(client.user))
+
+
+@client.event
+async def on_player_join(member):
+    print(f'{member} has joined a server')
+
+
+@client.event
+async def on_player_remove(member):
+    print(f'{member} has left/kicked from a server')
 
 @client.event
 async def on_message(message):
-  if discord.message.author== client.user:
-    return 
+    if message.content.startswith('$ping'):
+        await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
+    if message.content.startswith('$hi'):
+        await message.channel.send('Hello')
+    if message.content.startswith('$settings prefix'):
+        await message.channel.send('prefix changing mode')
+    if message.content.startswith('$clear'):
+        amount = 50
+        await message.channel.purge(limit = amount + 1)
 
-  context = message.content.strip().lower()
-  if context.startswith('$hello'):
-    await message.channel.send('Hello')
-
-client.run("TOKEN")
+client.run("Token")
